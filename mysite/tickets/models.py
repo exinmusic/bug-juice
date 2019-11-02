9f09db4fdc51fafc217f8939724fa18ad017ab6c
xn <exinmusic@gmail.com>
2019-11-01 19:22:50 -0700

    Mocking tickets

--- mysite/tickets/models.py
+++ mysite/tickets/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Report(models.Model):
+    nickname = models.CharField(max_length=25)
+    description = models.TextField(max_length=600)
+    time_reported = models.DateTimeField()
+    reporter = models.CharField(max_length=80)
+    
