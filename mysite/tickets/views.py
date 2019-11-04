70a978280ac0e0de91b156f865ab1754c52c0ea1
David <exinmusic@gmail.com>
2019-11-03 18:15:27 -0800

    Started report submission

--- mysite/tickets/views.py
+++ mysite/tickets/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from . import models
 
-def index(request):
+def tickets(request):
     reports = models.Report.objects.all().values()
-    return render(request, "tickets/index.html", {"reports":reports})
+    return render(request, "tickets/tickets.html", {"reports":reports})
+
+def submit(request):
+    return render(request, "tickets/submit.html")
