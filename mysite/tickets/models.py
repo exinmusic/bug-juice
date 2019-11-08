555285ca45952b939966f9d56dc6a47af9ce3e4c
xn <exinmusic@gmail.com>
2019-11-08 01:54:11 -0800

    Bug Assignment
    Bugs are removed from tickets, but need to appear on a bug list next.

--- mysite/tickets/models.py
+++ mysite/tickets/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
 TYPE_CHOICES = (
-	("bug","Bug"),
-    ("vulnerability","Vulnerability"),
-	("feature request","Feature Request"),
+	("Bug","Bug"),
+    ("Vulnerability","Vulnerability"),
+	("Feature request","Feature Request"),
 )
 
 DEPARTMENT_CHOICES = (
-    ("db","Database"),
-    ("auth","User Auth"),
-    ("styling","Styling"),
-    ("front","Front-end App"),
-    ("back","Back-end App"),
+    ("Database","Database"),
+    ("User Auth","User Auth"),
+    ("Styling","Styling"),
+    ("Front-end","Front-end"),
+    ("Back-end","Back-end"),
 )
 
 class Report(models.Model):
@@ -19,8 +19,8 @@ class Report(models.Model):
     description = models.TextField(max_length=600)
     time_reported = models.DateTimeField(auto_now_add=True, blank=True)
     reporter = models.CharField(max_length=80)
-    report_type = models.CharField(max_length=6, choices=TYPE_CHOICES, default='bug')
-    department = models.CharField(max_length=6, choices=DEPARTMENT_CHOICES, default='back')
+    report_type = models.CharField(max_length=16, choices=TYPE_CHOICES, default='bug')
+    department = models.CharField(max_length=16, choices=DEPARTMENT_CHOICES, default='back')
     bug = models.BooleanField(default=False)
     solved = models.BooleanField(default=False)
 
--- mysite/tickets/views.py
+++ mysite/tickets/views.py
@@ -3,16 +3,9 @@ from django.http import HttpResponse
 from . import models
 from datetime import datetime
 
-def tickets(request):
-    if request.method == "POST":
-        print(request.POST.get('manage'))
-        reports = models.Report.objects.all().values()
-        return render(request, "tickets/tickets.html", {"reports":reports}) 
-    else:
-        reports = models.Report.objects.all().values()
-        return render(request, "tickets/tickets.html", {"reports":reports})
-
+# SUBMIT TICKET
 def submit(request):
+    # POST
     if request.method == "POST":
         report_data = {}
         report_data['nickname'] = request.POST.get('nickname')
@@ -26,7 +19,30 @@ def submit(request):
 									department=report_data['department'],
 									description=report_data['description'],
                                     reporter=report_data['reporter'])
-        reports = models.Report.objects.all().values()                            
+        reports = models.Report.objects.all().filter(bug=False).values()                            
         return render(request, "tickets/tickets.html", {"reports":reports})
+    # GET
     else:
         return render(request, "tickets/submit.html")
+
+# MANAGE TICKETS
+def tickets(request):
+    # POST
+    if request.method == "POST":
+        manage_req = request.POST.get('manage')
+        report_id = request.POST.get('report_id')
+        print("Ticket management...")
+        if manage_req == "bug":
+            try:
+                entry = models.Report.objects.get(id=report_id)
+                entry.bug = True
+                entry.save()
+            except:
+                print('No report by that ID found...')
+
+            reports = models.Report.objects.all().filter(bug=False).values()
+            return render(request, "tickets/tickets.html", {"reports":reports})
+    # GET
+    else:
+        reports = models.Report.objects.all().filter(bug=False).values()
+        return render(request, "tickets/tickets.html", {"reports":reports})
