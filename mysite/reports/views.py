bac7be43571745a6b94ee0eabc0d07d064a6b97a
David <exinmusic@gmail.com>
2020-02-06 15:46:14 -0800

    Membership Check
    Members of one project can't view reports from another by simply incrementing id's in the URL.

--- mysite/reports/views.py
+++ mysite/reports/views.py
@@ -1,66 +1,81 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.exceptions import PermissionDenied
 from . import models
 
+# True if user is a member of a report's project.
+def report_member(user, rid, result = False):
+    if models.Report.objects.get(id=rid).project.profile_set.filter(id=user.profile.id):
+        result = True
+    return result
+
+
 @login_required
 def report(request, rid):
-    # Check user is member of this Project
-    r = models.Report.objects.get(id=rid)
-    if r.project.profile_set.filter(id=request.user.profile.id):
+    # CHECK MEMBERSHIP
+    if report_member(request.user,rid):
+
         # CURRENT REPORT
-        return render(request, "reports/report.html", {"report":r})
-    else:
-        raise PermissionDenied
+        return render(request, "reports/report.html", {"report":models.Report.objects.get(id=rid)})
+
+    else: raise PermissionDenied
+
 
 @login_required
 def manage(request, rid):
-    # POST
-    if request.method == "POST":
-        manage_req = request.POST.get('selection')
-        report_id = rid
-        print("Ticket management...")
+    # CHECK MEMBERSHIP
+    if report_member(request.user,rid):
 
-        # USERS SELECTS Confirm
-        if manage_req == "Confirm":
-            try:
+        # POST
+        if request.method == "POST":
+            manage_req = request.POST.get('selection')
+            report_id = rid
+            print("Ticket management...")
+
+            # USERS SELECTS Confirm
+            if manage_req == "Confirm":
+                try:
+                    entry = models.Report.objects.get(id=report_id)
+                    entry.confirmed = True
+                    entry.save()
+                    return redirect('/dashboard/tickets')
+                except:
+                    print('No report by that ID found...')
+
+            # USERS SELECTS Review
+            if manage_req == "Review":
+                return redirect("/reports/"+report_id)
+
+            # USERS SELECTS Close
+            if manage_req == "Close":
                 entry = models.Report.objects.get(id=report_id)
-                entry.confirmed = True
+                entry.delete()
+                return redirect("/dashboard/tickets")
+
+            # USERS SELECTS Solve
+            if manage_req == "Solve":
+                entry = models.Report.objects.get(id=report_id)
+                entry.solved = True
                 entry.save()
-                return redirect('/dashboard/tickets')
-            except:
-                print('No report by that ID found...')
+                return redirect("/dashboard/tickets")
 
-        # USERS SELECTS Review
-        if manage_req == "Review":
-            return redirect("/reports/"+report_id)
+    else: raise PermissionDenied
 
-        # USERS SELECTS Close
-        if manage_req == "Close":
-            entry = models.Report.objects.get(id=report_id)
-            entry.delete()
-            return redirect("/dashboard/tickets")
-
-        # USERS SELECTS Solve
-        if manage_req == "Solve":
-            entry = models.Report.objects.get(id=report_id)
-            entry.solved = True
-            entry.save()
-            return redirect("/dashboard/tickets")
 
 @login_required
 def comment(request, rid):
-    # CURRENT REPORT
-    r = models.Report.objects.get(id=rid)
+    # CHECK MEMBERSHIP
+    if report_member(request.user,rid):
+        # CURRENT REPORT
+        r = models.Report.objects.get(id=rid)
 
-    # POST
-    if request.method == "POST":
-        models.Comment.objects.create(
-            author = request.user,
-            text = request.POST.get('text'),
-            report = r
-        )
-        return render(request, "reports/report.html", {"report":r})
-
-        
+        # POST
+        if request.method == "POST":
+            models.Comment.objects.create(
+                author = request.user,
+                text = request.POST.get('text'),
+                report = r
+            )
+            return render(request, "reports/report.html", {"report":r})        
+    else: raise PermissionDenied
